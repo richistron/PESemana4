8c367c920a337291916173ef7d49ec19943d56b5
Ricardo Rivas <richistron@gmail.com>
2022-09-22 19:37:51 -0700

    Primer commit

---
+++ PESemana4/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+const string tema =
+    "Desarrolla un programa que recorra  los números del  1 al 100 y muestre en pantalla únicamente números pares o divisibles entre 3.";
+
+Console.WriteLine("!Bienvenido a la semana 4!");
+Console.WriteLine("El tema de hoy es: \n \"{0}\"", tema);
+
+int contador, limite;
+limite = 100;
+string salida = "";
+
+for (contador = 1; contador <= limite; contador++)
+    if (contador % 2 == 0 || contador % 3 == 0)
+        salida = salida == "" ? contador.ToString() : $"{salida} - {contador}";
+
+Console.WriteLine($"Resultado:\n{salida}");
---
+++ README.md
@@ -0,0 +1,9 @@
+﻿Tarea Semana 4
+==============
+
+* **Nombre de la Materia**: Programación Estructurada
+* **Nombre de la Licenciatura**: Ingeniría en Sistemas Computacionales
+* **Nombre del Alumno**: Ricardo Rivas González
+* **Nombre de la Tarea**: Puntos Extras Semana 4
+* **Nombre del Docente**: Sandra Castillo López
+* **Fecha**: 2022/09/22
